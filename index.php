cc8664f4e06486afc2eaa602a9149f026342e831
Martin Leroy <la179331@student.helha.be>
2019-07-09 16:24:41 +0200

    remove comment

--- index.php
+++ index.php
@@ -1,16 +1,4 @@
 <?php
-	/*$json = @file_get_contents('php://input');
-		$icxn = mysqli_connect("127.0.0.1", "tmaas_user", "zB40~2R4jw!=NH[n", "tmaas_database");
-	if (!empty($json)) {
-		header("Content-Type: application/json");
-		// JUST FOR DEBUG DON'T KILL ME :(
-		$v = addslashes($json);
-		$fuckme = (mysqli_query($icxn, "INSERT INTO post_json(value)VALUES('{$v}')"));
-			
-	} */
-
-
-
 	include 'inc.d/func.php';
 	include 'inc.d/settings.php';
 	include 'inc.d/model/location.class.php';
